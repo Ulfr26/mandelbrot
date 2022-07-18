2a5f25a085500447bca3612a6f0c858d3b1f5122
Liam Borella <ulfrthewolf@gmail.com>
2022-07-18 20:10:03 +1000

    Moved the camera code into its own module

---
+++ src/camera.rs
@@ -0,0 +1,69 @@
+use raylib::prelude::*;
+use crate::*;
+
+// Represents the position of the camera, and the dimensions of the viewport
+// (in the complex plane, not in pixel space)
+pub struct Camera {
+    pub dim: [f64; 2],
+    pub pos: [f64; 2],
+    pub iterations: i32,
+}
+
+impl Camera {
+    fn new(dim: [f64; 2], pos: [f64; 2], iterations: i32) -> Camera {
+        Camera {
+            dim,
+            pos,
+            iterations,
+        }
+    }
+
+    pub fn update(&mut self, rl: &RaylibHandle) {
+        let dt = rl.get_frame_time() as f64;
+        // Update psoition
+        // I know i misspelled that word but im keeping it cause its funni
+        let hdir = (rl.is_key_down(KeyboardKey::KEY_D) as i32
+            - rl.is_key_down(KeyboardKey::KEY_A) as i32) as f64;
+
+        let vdir = (rl.is_key_down(KeyboardKey::KEY_S) as i32
+            - rl.is_key_down(KeyboardKey::KEY_W) as i32) as f64;
+
+        let dx = hdir * SPEED * dt / WIDTH as f64;
+        let dy = vdir * SPEED * dt / HEIGHT as f64;
+
+        self.pos[0] += dx * self.dim[0];
+        self.pos[1] += dy * self.dim[1];
+
+        // Change iteration level
+        if rl.is_key_pressed(KeyboardKey::KEY_UP) {
+            self.iterations += ITER_SPEED;
+        }
+
+        if rl.is_key_pressed(KeyboardKey::KEY_DOWN) {
+            self.iterations -= ITER_SPEED;
+        }
+
+        self.iterations = self.iterations.max(0);
+
+        // Change zoom level
+        let scroll = rl.is_key_down(KeyboardKey::KEY_LEFT_SHIFT) as i32
+            - rl.is_key_down(KeyboardKey::KEY_SPACE) as i32;
+        if scroll > 0 {
+            self.dim[0] *= ZOOM;
+            self.dim[1] *= ZOOM;
+        } else if scroll < 0 {
+            self.dim[0] /= ZOOM;
+            self.dim[1] /= ZOOM;
+        }
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        Self::new(
+            [RANGE_X.1 - RANGE_X.0, RANGE_Y.1 - RANGE_Y.0],
+            [(RANGE_X.0 + RANGE_X.1) / 2., (RANGE_X.0 + RANGE_X.1) / 2.],
+            MAX_ITERATIONS
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod camera;
+
 use itertools::Itertools;
 use raylib::prelude::*;
 use rayon::prelude::*;
+use camera::Camera;
 
 // Default values
 const HUE: f32 = 252.;
@@ -13,72 +16,7 @@ const SPEED: f64 = 200.0;
 const ZOOM: f64 = 0.9; // Sheogorath!
 const ITER_SPEED: i32 = 64; // Add this many iterations per button press
 
-// Represents the position of the camera, and the dimensions of the viewport
-// (in the complex plane, not in pixel space)
-struct Camera {
-    dim: [f64; 2],
-    pos: [f64; 2],
-    iterations: i32,
-}
 
-impl Camera {
-    fn new(dim: [f64; 2], pos: [f64; 2], iterations: i32) -> Camera {
-        Camera {
-            dim,
-            pos,
-            iterations,
-        }
-    }
-
-    fn update(&mut self, rl: &RaylibHandle) {
-        let dt = rl.get_frame_time() as f64;
-        // Update psoition
-        // I know i misspelled that word but im keeping it cause its funni
-        let hdir = (rl.is_key_down(KeyboardKey::KEY_D) as i32
-            - rl.is_key_down(KeyboardKey::KEY_A) as i32) as f64;
-
-        let vdir = (rl.is_key_down(KeyboardKey::KEY_S) as i32
-            - rl.is_key_down(KeyboardKey::KEY_W) as i32) as f64;
-
-        let dx = hdir * SPEED * dt / WIDTH as f64;
-        let dy = vdir * SPEED * dt / HEIGHT as f64;
-
-        self.pos[0] += dx * self.dim[0];
-        self.pos[1] += dy * self.dim[1];
-
-        // Change iteration level
-        if rl.is_key_pressed(KeyboardKey::KEY_UP) {
-            self.iterations += ITER_SPEED;
-        }
-
-        if rl.is_key_pressed(KeyboardKey::KEY_DOWN) {
-            self.iterations -= ITER_SPEED;
-        }
-
-        self.iterations = self.iterations.max(0);
-
-        // Change zoom level
-        let scroll = rl.is_key_down(KeyboardKey::KEY_LEFT_SHIFT) as i32
-            - rl.is_key_down(KeyboardKey::KEY_SPACE) as i32;
-        if scroll > 0 {
-            self.dim[0] *= ZOOM;
-            self.dim[1] *= ZOOM;
-        } else if scroll < 0 {
-            self.dim[0] /= ZOOM;
-            self.dim[1] /= ZOOM;
-        }
-    }
-}
-
-impl Default for Camera {
-    fn default() -> Self {
-        Self::new(
-            [RANGE_X.1 - RANGE_X.0, RANGE_Y.1 - RANGE_Y.0],
-            [(RANGE_X.0 + RANGE_X.1) / 2., (RANGE_X.0 + RANGE_X.1) / 2.],
-            MAX_ITERATIONS
-        )
-    }
-}
 
 // does ????
 // stolen from wikipedia
